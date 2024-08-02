8d3d6782ab0bd5eaf161c25b4645b321826e7e4a
Jesus Bernat <contact@asedem.com>
2024-08-02 08:32:33 +0200

    Uso de la DLL

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
+#include <windows.h>
 using namespace std;
 
+typedef int (*MultFunc)(int x, int y);
+
 int main() {
+
+  
+  HMODULE hModule = LoadLibraryA("operaciones.dll");
+  MultFunc multiplica = (MultFunc)GetProcAddress(hModule, "multiplica");
+
+  //GetProcAddress(hModule,"multiplica");
   
   int a=3, b=7, res;
-  res = multiplica(a,b);
+  res = multiplica(5,3);
   cout << res;
 
   return 0;
